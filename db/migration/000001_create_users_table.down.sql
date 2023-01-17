7affd66bd4882c8ad842b089cc7c8781bb98f2e6
Kuchezai <Kozhenov.A.A@yandex.ru>
2023-01-17 18:00:49 +0300

    users table

--- db/migration/000001_create_users_table.down.sql
+++ db/migration/000001_create_users_table.down.sql
@@ -1,9 +1,1 @@
-CREATE TABLE users
-(
-    id integer NOT NULL,
-    login character varying(50) NOT NULL,
-    name character varying(50) NOT NULL,
-    pass character varying(50) NOT NULL,
-    email character varying(50) NOT NULL,
-    PRIMARY KEY (id)
-);
+DROP TABLE IF EXISTS users;
